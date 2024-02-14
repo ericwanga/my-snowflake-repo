211db712c42285e0477744489d79dde8e5613627
Eric Wang <84424184+ericwanga@users.noreply.github.com>
2024-02-15 10:27:18 +1030

    Update streamlit_app.py
    more tests

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.title('My Title')
 st.title('Another :blue[Blue Title] :sunglasses:')
 st.header('My Header')
-st.text('My text')
-st.text('Other texts')
+st.text('Some text')
+st.text('More texts')
