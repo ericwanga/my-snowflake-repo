1817a60afb10f945ae37c449d70895c3d7dc7f45
Eric Wang <84424184+ericwanga@users.noreply.github.com>
2024-02-15 18:01:46 +1030

    Update streamlit_app.py
    update position

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,6 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_df = my_fruit_df.set_index('Fruit')
 
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-st.text(fruityvice_response)
-
 # add multiselect, set 2 fruits as default selection
 fruits_selected = st.multiselect('Pick some fruits:', list(my_fruit_df.index), ['Avocado', 'Strawberries'])
 st.text(type(fruits_selected))
@@ -29,3 +26,6 @@ fruits_to_show = my_fruit_df.loc[fruits_selected]
 # st.dataframe(my_fruit_list)
 st.dataframe(fruits_to_show)
 
+# call fruityvice API (does not require key)
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+st.text(fruityvice_response)
