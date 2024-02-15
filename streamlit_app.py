dd6bfe677989e1a3d5ff763907a0998f3e4f89f4
Eric Wang <84424184+ericwanga@users.noreply.github.com>
2024-02-15 10:46:49 +1030

    Update streamlit_app.py
    change index col

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,10 @@ st.text('🥗 text')
 st.text('🐔 text')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt', index=False)
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-st.header('with Interaction')
 st.multiselect('Pick some fruits:', list(my_fruit_list.index))
+
 st.dataframe(my_fruit_list)
 
