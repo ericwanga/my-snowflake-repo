29ea9e6f7386d4606cb06dc9ee32831826762376
Eric Wang <84424184+ericwanga@users.noreply.github.com>
2024-02-15 10:56:09 +1030

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # load data as df
 my_fruit_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-my_fruit_df = my_fruit_list.set_index('Fruit')
+my_fruit_df = my_fruit_df.set_index('Fruit')
 
 # add multiselect, set 2 fruits as default selection
 fruits_selected = st.multiselect('Pick some fruits:', list(my_fruit_df.index), ['Avocado', 'Strawberries'])
-st.text(dtype(fruits_selected))
+st.text('dtype: ', dtype(fruits_selected))
 # filter df with selected fruits
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
